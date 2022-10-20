b831605ccdf90364ec64346317e24c86ee61167f
tricker-a1 <107811481+tricker-a1@users.noreply.github.com>
2022-10-20 22:49:39 +0300

    Update migration.sql

--- prisma/migrations/20221014191647_kudos/migration.sql
+++ prisma/migrations/20221014191647_kudos/migration.sql
@@ -13,6 +13,7 @@ CREATE TABLE "KudosTransaction" (
     "recipientId" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    
 
     CONSTRAINT "KudosTransaction_pkey" PRIMARY KEY ("id")
 );
