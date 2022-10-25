0c0f077e1d7bfd6e9c6d50a2c36c26638c9b6a3e
tricker-a1 <107811481+tricker-a1@users.noreply.github.com>
2022-10-25 13:59:14 +0300

    Update rollback.sh

--- rollback.sh
+++ rollback.sh
@@ -3,7 +3,7 @@ cd /home/runner/work/mvp/mvp/prisma/migrations
 ls -d */  > errors.txt
 for i in {1..5}
 do
-         $i=$(sed '$i!d ; s/.$//' errors.txt)
+         $i=$(sed '1!d ; s/.$//' errors.txt)
 done
 cd /home/runner/work/mvp/mvp
 for i in {1..5}
