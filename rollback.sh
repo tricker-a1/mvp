1105c2975459e4be40512944ba17c070c2443ee7
tricker-a1 <107811481+tricker-a1@users.noreply.github.com>
2022-10-25 13:54:52 +0300

    Update rollback.sh

--- rollback.sh
+++ rollback.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd /home/runner/work/mvp/mvp/prisma/migrations 
 ls -d */  > errors.txt
-for i in {5..0}
+for i in {1..5}
 do
          $i=$(sed '$i!d ; s/.$//' errors.txt)
 done
 cd /home/runner/work/mvp/mvp
-for i in {5..0}
+for i in {1..5}
 do
           npx prisma migrate resolve --rolled-back $$i || true
 done
